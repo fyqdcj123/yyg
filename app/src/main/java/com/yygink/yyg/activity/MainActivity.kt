575be9fde9b19e017383a50ab5a287ac63e6c012
mac <mac@macdeMacBook-Pro.local>
2021-12-23 10:07:05 +0800

    Initial commit

---
+++ app/src/main/java/com/yygink/yyg/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yygink.yyg.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yygink.yyg.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
